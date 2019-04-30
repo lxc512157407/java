7538d3558c2c4597c87efe35e48992ff6bc823d2
李现成 <512157407@qq.com>
2019-04-30 11:13:01 +0800

    小修改

--- src/com/learn/lesson/lesson1/PartTotal.java
+++ src/com/learn/lesson/lesson1/PartTotal.java
@@ -3,7 +3,6 @@ package com.learn.lesson.lesson1;
 public class PartTotal {
     public static void main(String[] args) {
         Part1 part1 =new Part1();
-//        第一部分的实例
         part1.say();
     }
 }

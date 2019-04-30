b3eb92b60a91b99b95e1600bee2b7dd20d55b1dc
李现成 <512157407@qq.com>
2019-04-30 10:58:04 +0800

    第一次Java提交

---
+++ src/com/learn/lesson/lesson1/Part1.java
@@ -0,0 +1,7 @@
+package com.learn.lesson.lesson1;
+
+public class Part1 {
+    public void say(){
+        System.out.println("这是第一部分");
+    }
+}
---
+++ src/com/learn/lesson/lesson1/PartTotal.java
@@ -0,0 +1,8 @@
+package com.learn.lesson.lesson1;
+
+public class PartTotal {
+    public static void main(String[] args) {
+        Part1 part1 =new Part1();
+        part1.say();
+    }
+}
